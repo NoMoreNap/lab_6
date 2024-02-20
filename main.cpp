65d5f42557acfb8de4ec0cf8d73dbb1e6b254023
NoMoreNap <vv2trash@gmail.com>
2024-02-20 15:27:19 +0300

    lab 6

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 
 using  namespace std;
 
-wstring get_month(int n) {
-    const wstring months[]={L"Январь",L"Февраль",L"Март",L"Апрель",L"Май",L"Июнь",L"Июль",L"Август",L"Сентябрь",L"Октябрь",L"Ноябрь",L"Декабрь"};
+wstring get_day(int n) {
+    const wstring days[]={L"Понедельник",L"Вторник",L"Среда",L"Четверг",L"Пятница",L"Суббота",L"Воскресенье",};
+
     switch (n) {
-        case 0:case 1:case 2:case 3:case 4:case 5:case 6:case 7:case 8:case 9:case 10:case 11:
-            return months[n];
+        case 0:case 1:case 2:case 3:case 4:case 5:case 6:
+            return days[n];
         default:
             return L"Введено неверное число";
     }
@@ -16,13 +17,13 @@ wstring get_month(int n) {
 int main() {
     setlocale(LC_ALL, "Russian");
     unsigned short int n = 0;
-    wcout << L"Введите номер месяца >> ";
+    wcout << L"Введите номер дня >> ";
     cin >> n;
     if(!cin.good()) {
         wcout << L"Введен неверный тип данных";
         return 0;
     }
-    const wstring month = get_month(n - 1);
-    wcout << month;
+    const wstring day = get_day(n - 1);
+    wcout << day;
     return 0;
 }
